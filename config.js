413542f9f5b2422d86f48f3d0e8cfc8308b81045
Robert Peacock <rjpeacock@gmail.com>
2026-02-22 14:09:10 -0500

    Addressing API variable names

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const API_KEYS = {
     CURRENCY_KEY: "b5af70e98175c3764fda6084",
-    AI_KEY: "AIzaSyAZ3XwcoNY6CxqY0gARn-YmFZPIj9a5084"
+    GEMINI_KEY: "AIzaSyAZ3XwcoNY6CxqY0gARn-YmFZPIj9a5084" // Renamed from AI_KEY
 };
